4e515a578d9d21e5f85f87a812ff1c5cb916e4f2
alazythe <eexepts@gmail.com>
2025-07-17 22:51:46 +0200

    Updates

--- _app/immutable/chunks/BsdKDzgS.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_15ne8ym?.base??"/mainframe",e=globalThis.__sveltekit_15ne8ym?.assets??s;export{e as a,s as b};
---
+++ _app/immutable/chunks/DLqIVyy9.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_kp5crd?.base??"/mainframe",a=globalThis.__sveltekit_kp5crd?.assets??s;export{a,s as b};
